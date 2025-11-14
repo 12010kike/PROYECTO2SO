78361b1c4888f7033edffdf492dd814ff432bfb5
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2025-11-14 15:08:51 -0400

    configuracion del json

--- PROYECTO2/src/sistemaarchivos/Planificacion/PlanificadorSSTF.java
+++ PROYECTO2/src/sistemaarchivos/Planificacion/PlanificadorSSTF.java
@@ -11,29 +11,33 @@ import sistemaarchivos.estructuras.ColaEnlazada;
  *
  * @author eabdf
  */
-public final class PlanificadorSSTF implements PlanificadorDisco{
-   private int posicion;
+public final class PlanificadorSSTF implements PlanificadorDisco {
+
+    private int posicion;
+    private int min;
+    private int max;
 
     @Override
-    public void configurar(int posicionActual, int min, int max) {
+    public void configurar(int posicionActual, int pistaMin, int pistaMax) {
         this.posicion = posicionActual;
+        this.min = pistaMin;
+        this.max = pistaMax;
     }
 
     @Override
     public SolicitudES seleccionarSiguiente(ColaEnlazada<SolicitudES> cola) {
         if (cola.estaVacia()) return null;
+
         final SolicitudES[] mejor = {null};
-        final int[] mejorDist = {Integer.MAX_VALUE};
+        final int[] mejorDelta = {Integer.MAX_VALUE};
 
         cola.recorrer((s, i) -> {
             int d = Math.abs(s.getPistaDestino() - posicion);
-            if (d < mejorDist[0]) {
-                mejorDist[0] = d;
-                mejor[0] = s;
-            }
+            if (d < mejorDelta[0]) { mejorDelta[0] = d; mejor[0] = s; }
         });
+
         if (mejor[0] != null) {
-            cola.removerPrimeraOcurrencia(mejor[0]);
+            cola.removerPrimeraOcurrencia(mejor[0]); // ← se REMUEVE de la cola aquí
         }
         return mejor[0];
     }
